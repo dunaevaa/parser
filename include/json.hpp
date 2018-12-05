e7f915d37b2fd45279ed17575bb169fddaa1ab2e
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2018-12-05 23:25:00 +0300

    Update json.hpp

--- include/json.hpp
+++ include/json.hpp
@@ -1,13 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 #include <any>
 
+using namespace std;
+
 class Json {
+private:
+    vector<any> keys;
+    vector<any> values;
+    vector<any> array;
+
 public:
     // Конструктор из строки, содержащей Json-данные.
     Json(const std::string& s);
     
     // Метод возвращает true, если данный экземпляр содержит в себе JSON-массив. Иначе false.
     bool is_array() const;
-   	// Метод возвращает true, если данный экземпляр содержит в себе JSON-объект. Иначе false.
+    // Метод возвращает true, если данный экземпляр содержит в себе JSON-объект. Иначе false.
     bool is_object() const;
 
     // Метод возвращает значение по ключу key, если экземпляр является JSON-объектом.
@@ -21,8 +32,123 @@ public:
     std::any& operator[](int index);
     
     // Метод возвращает объект класса Json из строки, содержащей Json-данные.
-    static Json parse(const std::string& s);
+    static Json parse(const std::string& s) {
+      int i=0;
+      string str="\0";
+      while(s[i] != '\0') {
+         if(s[i] != '\n' && s[i] != '\t' && s[i] != ' ')
+             str+=s[i];
+         i++;
+      }
+      Json cur(str);
+      return cur;
+    };
     
     // Метод возвращает объекта класса Json из файла, содержащего Json-данные в текстовом формате.
     static Json parseFile(const std::string& path_to_file);
 };
+
+Json::Json(const std::string& s){
+    int i=1;
+    string str;
+    if(s[0] == '[' && s[s.length()-1] == ']'){
+        while(s[i] != ']'){
+            if(s[i] == ',' || s[i] == ']'){
+                i++;
+                array.push_back(atoi(str.c_str()));
+                str="";
+            }
+            str+=s[i];
+            i++;
+        }
+        array.push_back(atoi(str.c_str()));
+        str="";
+    }
+    if(s[0] == '{' && s[s.length()-1] == '}')
+        while(i<s.length()-1){
+            if(s[i] == ',')
+                i++;
+            if(s[i] == '"'){
+                i++;
+                while(s[i] != '"'){
+                    str+=s[i];
+                    i++;
+                }
+                i++;
+                //добавление в ключ
+                keys.push_back(str);
+                //cout<<str<<endl;
+                str="";
+            }else if(s[i] == ':'){
+                i++;
+                if(s[i] == '"'){
+                    i++;
+                    while(s[i] != '"'){
+                        str+=s[i];
+                        i++;
+                    }
+                    i++;
+                    values.push_back(str);
+                    str="";
+                    //добавление в значения
+                }else if(s[i] == '['){
+                    while(s[i] != ']'){
+                        str+=s[i];
+                        i++;
+                    }
+                    str+=s[i];
+                    i++;
+                    Json cur(str);
+                    values.push_back(cur);
+                    //cout<<str<<endl;
+                    str="";
+                }else if(s[i] == 't' || s[i] == 'f'){
+                    bool test;
+                    if(s[i] == 'f') {
+                        test=false;
+                        values.push_back(test);
+                    }else{
+                        test=true;
+                        values.push_back(test);
+                    }
+                    while(s[i] != ','){
+                        str+=s[i];
+                        i++;
+                    }
+                    //cout<<str<<endl;
+                    i++;
+                    str="";
+                }else if(s[i] == '{'){
+                    while(s[i] != '}'){
+                        str+=s[i];
+                        i++;
+                    }
+                    str+=s[i];
+                    i++;
+                    Json cur1(str);
+                    values.push_back(cur1);
+                    //cout<<str<<endl;
+                    str="";
+                }else{
+                    while(s[i] != ','){
+                        str+=s[i];
+                        i++;
+                    }
+                    i++;
+                    values.push_back(atoi(str.c_str()));
+                    //cout<<str<<endl;
+                    str="";
+                }
+            }
+        }
+}
+any& Json::operator[](const std::string& key) {
+    for (int i = 0; i < keys.size(); i++) {
+        if (any_cast<string>(keys[i]) == key) {
+            return values[i];
+        }
+    }
+}
+std::any& Json::operator[](int index) {
+    return array[index];
+}
